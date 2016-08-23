e4e2940f47d8f43c9a78f99e903e425fb9c6c522
Jonatha Vasconcelos <jonatha.vasconcelos@adp.com>
2016-08-23 19:40:36 -0300

    Adding safe rm function

---
+++ safe_rm.sh
@@ -0,0 +1,34 @@
+function rm(){
+    local trash='/home/latam/.trash'
+    
+    if ((${#@} < 1)); then
+        echo "[safe rm]: missing operand"
+        echo "Try 'rm --help' for more information"
+        return 1
+    fi
+    
+    if [[ "$1" == "--help" ]]; then
+        echo "Usage: rm FILE..."
+        echo "Move files to ${trash} instead of delete them permanently."
+        return 0
+    fi
+    
+    echo "--- safe rm ---"
+    for file in $@; do
+    
+        if [[ -f "${file}" || -d "${file}" ]]; then
+            file="${file/%\//}"
+            mv -v "${file/%\//}" ${trash}/${file}.$(date +%s);
+            
+        else
+            echo "[safe rm]: cannot stat ‘${file}’: No such file or directory"
+            break
+        fi
+        
+    done
+    
+    echo
+    echo "Trash size: $(du -sh ${trash})"
+    echo "---------------"
+    
+}
