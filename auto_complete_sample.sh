9e8ea2ee59bb1601571dce332de1514f6df37ace
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2017-06-14 21:22:58 -0300

    Updating auto_complete_sample.sh

--- auto_complete_sample.sh
+++ auto_complete_sample.sh
@@ -1,13 +1,33 @@
+#
+#
+#                  DESCRIPTION
+#
 # This is a sample of autocomplete script which should be sourced to the shell session.
+# 
+# Usage:
+#    source auto_complete_sample.sh
 #
-# <possibilities> represents any command to generate a single line list of possibilities to the autocomplete.
-# <executable> is the binary/script to be run.
+# Variables:
+#   params_to_choose: is a list of possibilities to the autocomplete. 
+#   It can be a hardcoded list or a command to generate one.
 #
-# The function _script() can have any name.
+#   executable_file: is the binary/script to be run.
+#
+# The function name "_script()" is a arbitrary name.
+#
+# Result Examples:
+#   $ foo <TAB> <TAB>
+#   $ bar  bar1  bar2  barN  foo1  foo2  fooN 
+#
+#   $ foo b <TAB>
+#   $ foo bar <TAB>
+#   $ bar  bar1  bar2  barN
 
-_script()
-{
-  _script_commands=$(<possibilities>)
+declare -r executable_file="foo"
+declare -r params_to_choose="bar bar1 bar2 barN foo1 foo2 fooN"
+
+function _script() {
+  _script_commands=${params_to_choose}
 
   local cur prev
   COMPREPLY=()
@@ -16,4 +36,5 @@ _script()
 
   return 0
 }
-complete -F _script <executable>
+
+complete -F _script ${executable_file}
